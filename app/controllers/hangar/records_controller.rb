4a33352d8f335443c96f4ee49f7c9b958e03cad5
Brian <brian.rieck@homeadvisor.com>
2019-01-22 11:39:54 -0500

    add new routes to see the status of created data

--- app/controllers/hangar/records_controller.rb
+++ app/controllers/hangar/records_controller.rb
@@ -7,5 +7,14 @@ module Hangar
       Hangar.created_data.clear
       render json: {"delete": "success"}.to_json, status: 200
     end
+
+    def status
+      render json: Hangar.created_data.to_json, status: 200
+    end
+
+    def remove
+      Hangar.created_data.clear
+      render json: {"removal": "success"}.to_json, status: 200
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
         resources factory, only: [:new, :create], controller: 'resources'
       end
       delete '/', to: 'records#delete'
+      get '/status', to: 'records#status'
+      get '/remove', to: 'records#remove'
     end
   end
 end

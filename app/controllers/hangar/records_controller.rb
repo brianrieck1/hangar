8d4c0bc9d79b42a2a84e9f811fd7da8a7c53c2aa
Brian <brian.rieck@homeadvisor.com>
2019-01-22 16:43:35 -0500

    Better expception handling

--- app/controllers/hangar/records_controller.rb
+++ app/controllers/hangar/records_controller.rb
@@ -1,8 +1,10 @@
 module Hangar
   class RecordsController < ActionController::Base
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
     def delete
       Hangar.created_data.each do |key, value|
-        value.constantize.destroy(key)
+        value.constantize.find(key)&.destroy
       end
       Hangar.created_data.clear
       render json: {"delete": "success"}.to_json, status: 200
@@ -15,6 +17,11 @@ module Hangar
     def remove
       Hangar.created_data.clear
       render json: {"removal": "success"}.to_json, status: 200
+    end
+
+    def record_not_found(_e)
+      json = {"record_not_found": _e.to_s}.to_json
+      render json: json, status: 404
     end
   end
 end

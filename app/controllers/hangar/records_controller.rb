3ed168cff1069db5985fb289889a69cc04019606
Brian <brian.rieck@homeadvisor.com>
2019-01-23 13:26:40 -0500

    Update error handling of delete

--- app/controllers/hangar/records_controller.rb
+++ app/controllers/hangar/records_controller.rb
@@ -1,12 +1,22 @@
 module Hangar
   class RecordsController < ActionController::Base
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
     def delete
       Hangar.created_data.each do |key, value|
-        value.constantize.find(key)&.destroy
+        begin
+          value.constantize.find(key)&.destroy
+          Hangar.created_data.delete(key)
+        rescue ActiveRecord::RecordNotFound => e
+          Hangar.created_data.delete(key)
+          json = {"record_not_found": e.to_s}.to_json
+          render json: json, status: 404
+          return
+        rescue Exception => e
+          json = {"error": e.to_s}.to_json
+          render json: json, status: 500
+          return
+        end
       end
-      Hangar.created_data.clear
       render json: {"delete": "success"}.to_json, status: 200
     end
 
@@ -17,11 +27,6 @@ module Hangar
     def remove
       Hangar.created_data.clear
       render json: {"removal": "success"}.to_json, status: 200
-    end
-
-    def record_not_found(_e)
-      json = {"record_not_found": _e.to_s}.to_json
-      render json: json, status: 404
     end
   end
 end

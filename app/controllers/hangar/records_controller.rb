9106beeb35e7a9720f113970414f0b7995b56ef6
Brian <brian.rieck@homeadvisor.com>
2019-01-24 13:46:37 -0500

    Update error handling regex

--- app/controllers/hangar/records_controller.rb
+++ app/controllers/hangar/records_controller.rb
@@ -4,12 +4,13 @@ module Hangar
     def delete
       Hangar.created_data.each do |key, value|
         begin
-          value.constantize.find(key).destroy
+          value.constantize.find(key).delete
           Hangar.created_data.delete(key)
         rescue ActiveRecord::RecordNotFound => e
           Hangar.created_data.delete(key)
         rescue ActiveRecord::StatementInvalid => e
-          foriegn_key_ref = /ActiveRecord::StatementInvalid: Mysql2::Error: Cannot delete or update a parent row: a foreign key constraint fails\s\(`.*?`.`(.*?)`, CONSTRAINT `\w*` FOREIGN KEY \(`(.*)`\) REFERENCES `.*?`.* WHERE `.*?`.`.*?` = (\d+)/.match(e)
+          puts e.to_s
+          foriegn_key_ref = /Mysql2::Error: Cannot delete or update a parent row: a foreign key constraint fails\s\(`.*?`.`(.*?)`, CONSTRAINT `\w*` FOREIGN KEY \(`(.*)`\) REFERENCES `.*?`.* WHERE `.*?`.`.*?` = (\d+)/.match(e.to_s)
           foriegn_key_ref[1].constantize.find_by(foriegn_key_ref[2] => foriegn_key_ref[3]).destroy
           value.constantize.find(key).destroy
           Hangar.created_data.delete(key)

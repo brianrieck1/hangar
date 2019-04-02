9801bbd49820c68beebd5b8dd9da9e612ad89dd6
Brian <brian.rieck@homeadvisor.com>
2019-04-02 09:02:46 -0400

    Need to singularize and constantize the table name

--- app/controllers/hangar/records_controller.rb
+++ app/controllers/hangar/records_controller.rb
@@ -11,7 +11,7 @@ module Hangar
         rescue ActiveRecord::StatementInvalid => e
           puts e.to_s
           foriegn_key_ref = /Mysql2::Error: Cannot delete or update a parent row: a foreign key constraint fails\s\(`.*?`.`(.*?)`, CONSTRAINT `\w*` FOREIGN KEY \(`(.*)`\) REFERENCES `.*?`.* WHERE `.*?`.`.*?` = (\d+)/.match(e.to_s)
-          foriegn_key_ref[1].constantize.find_by(foriegn_key_ref[2] => foriegn_key_ref[3]).destroy
+          foriegn_key_ref[1].singularize.camelize.constantize.find_by(foriegn_key_ref[2] => foriegn_key_ref[3]).destroy
           value.constantize.find(key).destroy
           Hangar.created_data.delete(key)
         rescue Exception => e
